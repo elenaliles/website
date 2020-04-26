2a75e1b1f63dde6ddf76215f39dc5e722ec3b74b
Elena <elena.a.liles@gmail.com>
2020-04-25 18:40:05 -0700

    packaging files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal Website 
+Project to learn Flask/HTML/JS. Going to build a basic Resume type website maybe with a blog or something cool. 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='website',
+      version='0.1',
+      description='Todo',
+      url='Todo',
+      author='Todo',
+      author_email='eliles@chem.ubc.ca',
+      license='Todo',
+      packages=["core"],
+      zip_safe=False)
